382e1aed197f1858fa9a92869397fe695d678a16
rtedkyt <edwin.ky.tong@gmail.com>
2022-05-14 22:40:04 +0800

    Add support for Go

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,11 +1,12 @@
 local nvim_lsp = require("lspconfig")
+local lsp_util = require("lspconfig/util")
 
-local on_attach = function(client)
+local on_attach_rust = function(client)
     require("completion").on_attach(client)
 end
 
 nvim_lsp.rust_analyzer.setup({
-    on_attach = on_attach,
+    on_attach = on_attach_rust,
     settings = {
         ["rust-analyzer"] = {
             assist = {
@@ -21,5 +22,13 @@ nvim_lsp.rust_analyzer.setup({
         }
     }
 })
+
+nvim_lsp.gopls.setup {
+    settings = {
+        cmd = { "gopls", "serve" },
+        filetypes = { "go", "gomod" },
+        root_dir = lsp_util.root_pattern("go.mod", ".git"),
+    }
+}
 
 vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)]])
