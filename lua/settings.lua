5d076325fffa4f8e11f74ef78a4b242ed73ed46e
rtedkyt <edwin.ky.tong@gmail.com>
2022-05-17 00:05:20 +0800

    Remove esc shortcut

--- lua/settings.lua
+++ lua/settings.lua
@@ -28,11 +28,11 @@ cmd([[
 ]])
 
 g.mapleader = ";"
-map("i", ";;", "<Esc>", { noremap = true, silent = true })
 map("n", "<leader>l", ":NvimTreeRefresh<CR>", { noremap = true, silent = true })
 map("n", "<leader>k", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 map("n", "<leader>m", ":ToggleTerm size=15 dir=~/Projects direction=horizontal<CR>", { noremap = true, silent = true })
-map("n", "<leader>n", ":TagbarToggle<CR>", { noremap = true, silent = true })
+map("n", "<leader>n", ":ToggleTermToggleAll", { noremap = true, silent = true })
+map("n", "<leader>b", ":TagbarToggle<CR>", { noremap = true, silent = true })
 
 -- Auto exit if NvimTree is the last one in buffer
 cmd("autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif")
